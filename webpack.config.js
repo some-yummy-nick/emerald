25bcc12a80b52ff6f1a55c44c8a22ef6d9600ef0
Marat Shaymardanov <yeffasol@mail.ru>
2017-06-27 13:09:13 +0300

    Add normalize

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
-const path = require('path');
+const path = require('path'),
+  ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+let NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
 
   entry: {
@@ -8,14 +12,33 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dest/js/'),
-    library:['name']
+    library: ['name']
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: "babel-loader"
-    }]
-  }
-
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            "css-loader"
+          ],
+          publicPath: "/dest"
+        })
+      },
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "../styles/vendor.css",
+      disable: false,
+      allChunks: true
+    })
+  ],
+  watch: NODE_ENV === 'development'
 };
