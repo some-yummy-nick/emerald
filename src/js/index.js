0a06c6f22ed1176392f68539da90bca66186ba43
Marat Shaymardanov <yeffasol@mail.ru>
2017-08-01 20:38:47 +0300

    Add success block

--- src/js/index.js
+++ src/js/index.js
@@ -14,20 +14,39 @@ $('.slider').slick({
 
 var review = document.querySelectorAll(".review");
 var overlay = document.querySelector(".overlay");
-let popupClose = document.querySelectorAll(".popup__close");
+let close = document.querySelectorAll(".close");
+let form = document.querySelectorAll(".popup__form");
 
 review.forEach(function (item) {
   item.addEventListener("click", function () {
     let popup = this.parentNode.parentNode.querySelector(".popup");
+    let close = this.parentNode.parentNode.querySelector(".success .close");
+    close.classList.add("js-visible");
+    close.classList.remove("js-hided");
     popup.classList.add("js-visible");
     popup.classList.remove("js-hided");
     overlay.style.height = 100 + "%";
   })
 });
-popupClose.forEach(function (item, i, arr) {
+
+form.forEach(function (item, i, arr) {
+  item.addEventListener("submit", function (e) {
+    let success = item.parentNode.parentNode.querySelector(".success");
+    let popup = item.parentNode.parentNode.querySelector(".popup");
+    popup.classList.add("js-hided");
+    popup.classList.remove("js-visible");
+    success.classList.add("js-visible");
+    success.classList.remove("js-hided");
+    overlay.style.height = 100 + "%";
+    e.preventDefault();
+  })
+});
+
+close.forEach(function (item, i, arr) {
   item.addEventListener("click", function () {
     item.parentNode.classList.add("js-hided");
     item.parentNode.classList.remove("js-visible");
+    this.classList.add("js-hided");
     overlay.style.height = 0;
   })
 });
